8a19903d0fc350e4c53421569028c13c181477f2
Zain Jamil <itszainjamil@gmail.com>
2023-06-23 19:51:09 +0500

    static version completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  return <AgeCalculator />;
+}
+
+export default App;
+
+function AgeCalculator() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Inputs />
+      <Button />
+      <Output />
     </div>
   );
 }
 
-export default App;
+function Inputs() {
+  return (
+    <div className="inputs">
+      <div>
+        <p className="input-title">Day</p>
+        <input className="input" type="number" placeholder="DD" />
+        <p className="error-text">This field is required</p>
+      </div>
+      <div>
+        <p className="input-title">Month</p>
+        <input className="input" type="number" placeholder="MM" />
+        <p className="error-text">This field is required</p>
+      </div>
+      <div>
+        <p className="input-title">Year</p>
+
+        <input className="input" type="number" placeholder="YYYY" />
+        <p className="error-text">This field is required</p>
+      </div>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div className="btn-container">
+      <hr className="horizontal-line" />
+      {/* <button className="btn">Submit</button> */}
+      <button className="btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="42"
+          height="39"
+          viewBox="0 0 46 44"
+        >
+          <g fill="none" stroke="#FFF" stroke-width="2">
+            <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
+          </g>
+        </svg>
+      </button>
+    </div>
+  );
+}
+
+function Output() {
+  return (
+    <div className="output-container">
+      <p className="output">
+        <span className="output-number">--</span> years
+      </p>
+      <p className="output">
+        <span className="output-number">--</span> months
+      </p>
+      <p className="output">
+        <span className="output-number">--</span> days
+      </p>
+    </div>
+  );
+}
