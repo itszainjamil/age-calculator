7b37b7e53f5ed8bb6a7f5a913e2df2e1ac7f1972
Zain Jamil <itszainjamil@gmail.com>
2023-06-25 01:53:20 +0500

    final version: added all functionalities, logics & handled all errors

--- src/App.js
+++ src/App.js
@@ -7,74 +7,199 @@ function App() {
 export default App;
 
 function AgeCalculator() {
-  return (
-    <div className="container">
-      <Inputs />
-      <Button />
-      <Output />
-    </div>
-  );
-}
-
-function Inputs() {
+  // User input
   const [day, setDay] = useState("");
   const [month, setMonth] = useState("");
   const [year, setYear] = useState("");
 
-  console.log(day, month, year);
+  // Error text for each input
+  const [errDay, setErrDay] = useState("");
+  const [errMonth, setErrMonth] = useState("");
+  const [errYear, setErrYear] = useState("");
+
+  // Calculated age according to the given input
+  const [ageYears, setAgeYears] = useState("--");
+  const [ageMonths, setAgeMonths] = useState("--");
+  const [ageDays, setAgeDays] = useState("--");
+
+  // Trigger handle click by pressing the enter key
+  function handleKeyPress(e) {
+    if (e.keyCode === 13) {
+      handleClick();
+    }
+  }
+
+  // When button is clicked
+  function handleClick() {
+    // Maximum number of days in each month
+    const ListOfDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // If The day number is greater than 31
+    if (day > 31) setErrDay("Must be a valid day");
+    // If input field is empty
+    else if (!day) setErrDay("This is a required field");
+    // If the date is invalid e.g. 31/04/1991 (there are 30 days in April)
+    else if (day > ListOfDays[month - 1]) setErrDay("Must be a valid day");
+    // If no error
+    else setErrDay("");
+
+    // If input field is empty
+    if (!month) setErrMonth("This is a required field");
+    // The month number is greater than 12
+    else if (month > 12) setErrMonth("Must be a valid month");
+    else setErrMonth("");
+
+    // If input field is empty
+    if (!year) setErrYear("This is a required field");
+    // The year is in the future
+    else if (year > 2023) setErrYear("Must be in the past");
+    // If no error
+    else setErrYear("");
+
+    // Creating a date object based on user input
+    const inputDate = new Date(`${month}-${day}-${year}`);
+
+    // Checking if the date is valid
+    const isValidDate = Date.parse(inputDate);
+
+    // If date is not valid or one of the fields is empty
+    if (isNaN(isValidDate) || day === "" || month === "" || year === "") {
+      setAgeDays("--");
+      setAgeYears("--");
+      setAgeMonths("--");
+      return;
+    }
+
+    // If no error then calculate the age and set states accordingly
+    calculateAge(inputDate);
+  }
+
+  function calculateAge(inputDate) {
+    // Convert the date strings to Date objects
+
+    // User's date
+    const start = new Date(inputDate);
+
+    // Current date
+    const end = new Date();
+
+    // Calculate the difference in milliseconds
+    const diff = Math.abs(end - start);
+
+    // Calculate the number of milliseconds in a year, month, and day
+    const msPerYear = 1000 * 60 * 60 * 24 * 365.25;
+    const msPerMonth = msPerYear / 12;
+    const msPerDay = 1000 * 60 * 60 * 24;
+
+    // Calculate and set the number of years, months, and days
+    setAgeYears(Math.floor(diff / msPerYear));
+    setAgeMonths(Math.floor((diff % msPerYear) / msPerMonth));
+    setAgeDays(Math.floor((diff % msPerMonth) / msPerDay));
+  }
 
   return (
+    <div className="container">
+      <Inputs
+        handleKeypress={handleKeyPress}
+        day={day}
+        setDay={setDay}
+        month={month}
+        setMonth={setMonth}
+        year={year}
+        setYear={setYear}
+        errDay={errDay}
+        errMonth={errMonth}
+        errYear={errYear}
+      />
+      <Button day={day} month={month} year={year} handleClick={handleClick} />
+      <Output ageYears={ageYears} ageMonths={ageMonths} ageDays={ageDays} />
+    </div>
+  );
+}
+
+function Inputs({
+  handleKeypress,
+  day,
+  setDay,
+  month,
+  setMonth,
+  year,
+  setYear,
+  errDay,
+  errMonth,
+  errYear,
+}) {
+  return (
+    // Container for all three inputs
     <div className="inputs">
+      {/* Each individual input div containing the title, input field and the error message */}
       <div>
-        <p className="input-title">Day</p>
+        {/* If there is an error, add the red color to the label */}
+        <p className={`input-title ${errDay !== "" ? "error-color" : ""}`}>
+          Day
+        </p>
         <input
+          onKeyDown={handleKeypress}
+          // If there is an error, add red outline to the input field
+          className={`input ${errDay !== "" ? "input-error" : "input-normal"}`}
           value={day}
           onChange={(e) => {
-            setDay(Number(e.target.value));
+            setDay(Number(e.target.value) < 0 ? day : Number(e.target.value));
           }}
-          className="input"
           type="number"
           placeholder="DD"
         />
-        <p className="error-text">This field is required</p>
+        {/* If there is an error then show the error text */}
+        {errDay !== "" && <p className="error-text">{errDay}</p>}
       </div>
       <div>
-        <p className="input-title">Month</p>
+        <p className={`input-title ${errMonth !== "" ? "error-color" : ""}`}>
+          Month
+        </p>
         <input
+          onKeyDown={handleKeypress}
+          className={`input ${
+            errMonth !== "" ? "input-error" : "input-normal"
+          }`}
           value={month}
           onChange={(e) => {
-            setMonth(Number(e.target.value));
+            setMonth(
+              Number(e.target.value) < 0 ? month : Number(e.target.value)
+            );
           }}
-          className="input"
           type="number"
           placeholder="MM"
         />
-        <p className="error-text">This field is required</p>
+        {errMonth !== "" && <p className="error-text">{errMonth}</p>}
       </div>
       <div>
-        <p className="input-title">Year</p>
+        <p className={`input-title ${errYear !== "" ? "error-color" : ""}`}>
+          Year
+        </p>
 
         <input
+          onKeyDown={handleKeypress}
+          className={`input ${errYear !== "" ? "input-error" : "input-normal"}`}
           value={year}
           onChange={(e) => {
-            setYear(Number(e.target.value));
+            setYear(Number(e.target.value) < 0 ? year : Number(e.target.value));
           }}
-          className="input"
           type="number"
           placeholder="YYYY"
         />
-        <p className="error-text">This field is required</p>
+        {errYear !== "" && <p className="error-text">{errYear}</p>}
       </div>
     </div>
   );
 }
 
-function Button() {
+function Button({ handleClick }) {
   return (
     <div className="btn-container">
+      {/* Horizontal line with the button */}
       <hr className="horizontal-line" />
-      {/* <button className="btn">Submit</button> */}
-      <button className="btn">
+      <button onClick={handleClick} className="btn">
+        {/* Icon inside the button */}
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="42"
@@ -90,17 +215,22 @@ function Button() {
   );
 }
 
-function Output() {
+function Output({ ageDays, ageMonths, ageYears }) {
   return (
+    // Contains all the three lines of output
     <div className="output-container">
+      {/* Each individual output */}
       <p className="output">
-        <span className="output-number">--</span> years
+        <span className="output-number">{ageYears} </span>
+        years
       </p>
       <p className="output">
-        <span className="output-number">--</span> months
+        <span className="output-number">{ageMonths} </span>
+        months
       </p>
       <p className="output">
-        <span className="output-number">--</span> days
+        <span className="output-number">{ageDays} </span>
+        days
       </p>
     </div>
   );
